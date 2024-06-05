cedbb2b6ca0516949c134e1716d714a73ff9d57d
Carmen Pop <p.carmenluiza@gmail.com>
2024-06-05 14:35:22 +0100

    Added a comment in App.test,is

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Hello this a Comment
+
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
